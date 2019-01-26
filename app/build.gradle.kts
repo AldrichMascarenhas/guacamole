b8040d2b4ee4761b5687631fbb4d2fac80db771a
Aldrich <mascarenhasaldrich@gmail.com>
2019-01-26 20:37:29 +0000

    Add temp master - detail activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    compileSdkVersion (28)
+    compileSdkVersion(28)
     defaultConfig {
         applicationId = "com.example.guacamole"
         minSdkVersion(21)
@@ -27,12 +27,17 @@ dependencies {
 
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
 
-    implementation ("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.11")
-    implementation ("androidx.appcompat:appcompat:1.0.2")
-    implementation ("androidx.core:core-ktx:1.1.0-alpha03")
-    implementation ("androidx.constraintlayout:constraintlayout:1.1.3")
-    implementation ("com.google.android.material:material:1.0.0")
-    testImplementation ("junit:junit:4.12")
-    androidTestImplementation ("androidx.test:runner:1.1.1")
-    androidTestImplementation ("androidx.test.espresso:espresso-core:3.1.1")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.11")
+    implementation("androidx.appcompat:appcompat:1.0.2")
+    implementation("androidx.core:core-ktx:1.1.0-alpha03")
+    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+    implementation("com.google.android.material:material:1.0.0")
+    implementation ("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation ("androidx.recyclerview:recyclerview:1.0.0")
+
+    implementation ("androidx.coordinatorlayout:coordinatorlayout:1.0.0")
+
+    testImplementation("junit:junit:4.12")
+    androidTestImplementation("androidx.test:runner:1.1.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.1.1")
 }
