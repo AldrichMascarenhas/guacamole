b67c4e5ab1646e4623c63ec5bd9bc8b97138bd6b
Aldrich <mascarenhasaldrich@gmail.com>
2019-01-28 00:09:00 +0000

    Create DI for Remote Data Source

---
+++ core/src/main/java/com/example/core/di/DataSource.kt
@@ -0,0 +1,46 @@
+package com.example.core.di
+
+import com.example.core.api.SpotifyAPIService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.dsl.module.module
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+
+val remoteDataSource = module {
+
+    /**
+     * Create single [OkHttpClient]
+     */
+    single { createOkHttpClient() }
+
+    single { createRetrofitService<SpotifyAPIService>(get(), getProperty(SPOTIFY_BASE_URL)) }
+
+
+}
+
+
+const val SPOTIFY_BASE_URL = "xyz"
+
+
+fun createOkHttpClient(): OkHttpClient {
+    val httpLoggingInterceptor = HttpLoggingInterceptor()
+    httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BASIC
+    return OkHttpClient.Builder()
+        .connectTimeout(60L, TimeUnit.SECONDS)
+        .readTimeout(60L, TimeUnit.SECONDS)
+        .addInterceptor(httpLoggingInterceptor).build()
+}
+
+
+inline fun <reified T> createRetrofitService(okHttpClient: OkHttpClient, url: String): T {
+    val retrofit = Retrofit.Builder()
+        .baseUrl(url)
+        .client(okHttpClient)
+        .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create()).build()
+    return retrofit.create(T::class.java)
+}
