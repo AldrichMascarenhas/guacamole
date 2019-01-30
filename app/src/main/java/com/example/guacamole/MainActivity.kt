61cc14e92b5ce7ddd728d3cc7c4231af69b6d5c8
Aldrich <mascarenhasaldrich@gmail.com>
2019-01-30 17:17:10 +0000

    Add Navigation Component

--- app/src/main/java/com/example/guacamole/MainActivity.kt
+++ app/src/main/java/com/example/guacamole/MainActivity.kt
@@ -1,26 +1,68 @@
 package com.example.guacamole
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration : AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Set Toolbar
+        val toolbar = findViewById<Toolbar>(R.id.main_activity_toolbar)
+
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+        val host: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.main_activity_nav_host_fragment) as NavHostFragment? ?: return
+
+        // Get Navigation Controller
+        val navController = host.navController
+        //Initialise AppBarConfiguration
+        appBarConfiguration = AppBarConfiguration.Builder(setOf(R.id.discoverFragment, R.id.movieFragment,R.id.tvFragment, R.id.searchFragment, R.id.favouriteFragment)).build()
+
+        setupActionBar(navController, appBarConfiguration)
+
+        setupBottomNavMenu(navController)
+
+
     }
 
+
+    private fun setupBottomNavMenu(navController: NavController) {
+        val bottomNav = findViewById<BottomNavigationView>(R.id.main_activity_bottom_nav_view)
+        bottomNav?.setupWithNavController(navController)
+    }
+
+    private fun setupNavigationMenu(navController: NavController) {
+//        // In split screen mode, you can drag this view out from the left
+//        // This does NOT modify the actionbar
+      //  val sideNavView = findViewById<NavigationView>(R.id.nav_view)
+       // sideNavView?.setupWithNavController(navController)
+    }
+
+    private fun setupActionBar(navController: NavController,
+                               appBarConfig : AppBarConfiguration) {
+//        // This allows NavigationUI to decide what label to show in the action bar
+//        // By using appBarConfig, it will also determine whether to
+//        // show the up arrow or drawer menu icon
+
+        setupActionBarWithNavController(navController, appBarConfig)
+    }
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
