d59aa05b22e8857b7d5b5a4485b6bc74be1e1902
Aldrich <mascarenhasaldrich@gmail.com>
2019-02-13 18:39:50 +0000

    Setup Koin Modules, koin.prop file

--- core/src/main/java/com/example/core/di/DataSource.kt
+++ core/src/main/java/com/example/core/di/DataSource.kt
@@ -1,8 +1,14 @@
 package com.example.core.di
 
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
 import com.example.core.api.MovieDatabaseAPIService
+import com.example.core.api.interceptor.AuthInterceptor
+import com.example.core.data.database.GuacamoleDatabase
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.android.ext.koin.androidApplication
 import org.koin.dsl.module.module
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
@@ -12,25 +18,30 @@ import java.util.concurrent.TimeUnit
 
 val remoteDataSourceDIModule = module {
 
+    single { createAuthInterceptor() }
+
     /**
      * Create single [OkHttpClient]
      */
-    single { createOkHttpClient() }
+    single { createOkHttpClient(authInterceptor = get()) }
 
-    single { createRetrofitService<MovieDatabaseAPIService>(get(), getProperty(MOVIE_DATABASE_API_BASE_URL)) }
+    single { createRetrofitService<MovieDatabaseAPIService>(get(), getProperty("MOVIE_DATABASE_API_BASE_URL")) }
+
+    single { createGuacamoleDatabase(androidApplication()) }
+
 
 }
 
+fun createAuthInterceptor() = AuthInterceptor()
 
-const val MOVIE_DATABASE_API_BASE_URL = "xyz"
 
-
-fun createOkHttpClient(): OkHttpClient {
+fun createOkHttpClient(authInterceptor: AuthInterceptor): OkHttpClient {
     val httpLoggingInterceptor = HttpLoggingInterceptor()
-    httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BASIC
+    httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
     return OkHttpClient.Builder()
         .connectTimeout(60L, TimeUnit.SECONDS)
         .readTimeout(60L, TimeUnit.SECONDS)
+        .addInterceptor(authInterceptor)
         .addInterceptor(httpLoggingInterceptor).build()
 }
 
@@ -43,3 +54,6 @@ inline fun <reified T> createRetrofitService(okHttpClient: OkHttpClient, url: St
         .addCallAdapterFactory(RxJava2CallAdapterFactory.create()).build()
     return retrofit.create(T::class.java)
 }
+
+fun createGuacamoleDatabase(context: Context): GuacamoleDatabase =
+    Room.databaseBuilder(context, GuacamoleDatabase::class.java, "guacamole_db").build()
