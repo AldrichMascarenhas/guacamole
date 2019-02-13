614c630333b8b0ee367b08d2934dcfec3636b32f
Aldrich <mascarenhasaldrich@gmail.com>
2019-02-13 18:40:06 +0000

    Start Koin in Android Application

--- app/src/main/java/com/example/guacamole/Guacamole.kt
+++ app/src/main/java/com/example/guacamole/Guacamole.kt
@@ -2,7 +2,10 @@ package com.example.guacamole
 
 import android.app.Application
 import com.example.core.di.coreDIModules
+import com.example.core.di.mapperModule
 import com.example.core.di.remoteDataSourceDIModule
+import com.example.core.di.repositoryModule
+import com.example.guacamole.di.viewmodels.viewModelModules
 import org.koin.android.ext.android.startKoin
 
 class Guacamole : Application() {
@@ -10,7 +13,11 @@ class Guacamole : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        startKoin(this, listOf(coreDIModules, remoteDataSourceDIModule))
+        startKoin(
+            this,
+            listOf(coreDIModules, remoteDataSourceDIModule, repositoryModule, mapperModule, viewModelModules),
+            loadPropertiesFromFile = true
+        )
 
 
     }
